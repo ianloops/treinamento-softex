7c78c56dbfbdee2d11d6d94e6fa061c142707a77
ianloops <ianllopesc@gmail.com>
2022-08-06 16:35:42 -0300

    exercicio 8

--- logica-e-poo/modulo-2/exercicio-8/pandas.py
+++ logica-e-poo/modulo-2/exercicio-8/pandas.py
@@ -2,11 +2,14 @@ import pandas as pd
 
 df = pd.read_csv("notas_alunos.csv")
 df["media"] = (df["nota_1"] + df ["nota_2"])/2
-df.loc[df["media"] >=7 and df["faltas"] <=5 , "situacao"] = "APROVADO"
-df.loc[df["media"] <7 or df["faltas"] >5, "situacao"] = "REPROVADO"
+df.loc[((df["media"] >=7) & (df["faltas"] <=5)) , "situacao"] = "APROVADO"
+df.loc[((df["media"] <7) | (df["faltas"] >5)) , "situacao"] = "REPROVADO"
 
 df.to_csv('alunos_situacao.csv', index = False)
 
-print(df["faltas"].max())
-print(df["media"].median())
-print(df["media"].max())
+print(df.head())
+
+print("\n")
+print("Maior número de Faltas: "+str(df["faltas"].max()))
+print("Média da Turma: "+str(df["media"].mean()))
+print("Maior Média: "+str(df["media"].max()))
