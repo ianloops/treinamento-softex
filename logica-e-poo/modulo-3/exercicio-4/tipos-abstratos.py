d39546d89d2e9d6205363c79df1154b83a4c226f
ianloops <ianllopesc@gmail.com>
2022-08-13 14:59:44 -0300

    exercicio 4

---
+++ logica-e-poo/modulo-3/exercicio-4/tipos-abstratos.py
@@ -0,0 +1,34 @@
+class numeroComplexo:
+    def _init_(self, n1, n2, n3, n4, n5, n6):
+        self.num1 = complex(n1, n2)
+        self.num2 = complex(n3, n4)
+        self.num3 = complex(n5, n6)
+
+    def realImag(self):
+        print("PARTES REAIS:")
+        print(self.num1.real, self.num2.real, self.num3.real)
+        print("PARTES IMAGINARIAS:")
+        print(self.num1.imag, self.num2.imag, self.num3.imag)
+
+    def soma(self):
+        soma = self.num1 + self.num2 + self.num3
+        print(f"Soma {soma}")
+
+    def subtracao(self):
+        subtracao = self.num1 - self.num2 - self.num3
+        print(f"Subtracao: {subtracao}")
+
+    def multiplicacao(self):
+        multiplicacao = self.num1 * self.num2 * self.num3
+        print(f"Multiplicação: {multiplicacao}")
+
+    def divisao(self):
+        divisao = self.num1 / self.num2 / self.num3
+        print(f"Divisão: {divisao}")
+
+    def tudo(self):
+        self.realImag()
+        self.soma()
+        self.subtracao()
+        self.multiplicacao()
+        self.divisao()
