4f606d20858807cf9a50d927bf68cc7e1fbd05a0
ianloops <ianllopesc@gmail.com>
2022-08-13 15:52:20 -0300

    exercicio 5

---
+++ logica-e-poo/modulo-4/exercicio-5/string1.py
@@ -0,0 +1,9 @@
+#Estruture três códigos, os quais devem conter uma função de manipulação
+#de string que obtenha resultado.
+
+#Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e
+#compartilhe o link desse projeto no campo ao lado para que outros
+#desenvolvedores possam analisá-lo.
+
+string="cachorro"
+print("A string tem "+str(len(string))+" Caracteres")
---
+++ logica-e-poo/modulo-4/exercicio-5/string2.py
@@ -0,0 +1,10 @@
+#Estruture três códigos, os quais devem conter uma função de manipulação
+#de string que obtenha resultado.
+
+#Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e
+#compartilhe o link desse projeto no campo ao lado para que outros
+#desenvolvedores possam analisá-lo.
+
+string = "Cães e Gatos"
+string = string.replace("Gatos", "Ratos")
+print(string)
