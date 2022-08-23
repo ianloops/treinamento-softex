4a6b6bad3ac24f8a82885ddf0886995b3c1697b9
ianloops <ianllopesc@gmail.com>
2022-08-23 14:55:14 -0300

    exercicio 2

---
+++ javascript/modulo-3/exercicio-2/index.js
@@ -0,0 +1,16 @@
+function soma(){
+    return num1+num2
+}
+
+function subtracao(n1, n2){
+    return n1-n2;
+}
+
+const multiplicacao = (n1, n2) => n1*n2;
+
+const num1=1;
+const num2=2;
+
+console.log(soma());
+console.log(subtracao(num1, num2));
+console.log(multiplicacao(num1, num2));
